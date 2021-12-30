61f14e73f23977da19a0f2c3337b9cf4f02e3b75
doruksahin <doruk.sahin@inavitas.com>
2021-12-30 09:42:24 +0300

    CORS problem fixed, handled by backend.

--- src/components/Chart.js
+++ src/components/Chart.js
@@ -4,30 +4,19 @@ import HighchartsReact from 'highcharts-react-official';
 var Highcharts = require('highcharts/highstock');
 // Load Highcharts Maps as a module
 require('highcharts/modules/map')(Highcharts);
-
-const Binance = require('node-binance-api');
-const binance = new Binance().options({
-    'APIKEY': 'kfZriXtkRzHcxBnECfS4in9QEDwmJL636vOSuVClPuDhjVfdByLWD5tzVPWZ3Nzs',
-    'APISECRET': 'lQUq5Nd34Dgcv8UsGZlD9YMAKCctEySkvZhBWlshxCrCCkuERrSXQiMD1NqwHj6n'
-});
+const axios = require('axios');
 
 function Chart() {
     const [ohlc, setOhlc] = useState([]);
     const [volume, setVolume] = useState([]);
-    const [ready, setReady] = useState(false);
     const [groupingUnits, setGroupingUnits] = useState([]);
-    const [data, setData] = useState([
-        [1553088600000, 186.23, 189.49, 184.73, 188.16, 31035200],
-        [1553175000000, 190.02, 196.33, 189.81, 195.09, 51034200],
-        [1553261400000, 195.34, 197.69, 190.78, 191.05, 42407700],
-        [1553520600000, 191.51, 191.98, 186.6, 188.74, 43845300],
-        [1553607000000, 191.66, 192.88, 184.58, 186.79, 49800500],
-        [1553693400000, 188.75, 189.76, 186.55, 188.47, 29848400],
-        [1553779800000, 188.95, 189.56, 187.53, 188.72, 20780400],
-        [1553866200000, 189.83, 190.08, 188.54, 189.95, 23548400]
-    ]);
+    const [data, setData] = useState([]);
 
-
+    useEffect(() => {
+        axios.get('http://localhost:3000/api').then(resp => {
+            setData(resp.data);
+        });
+    }, [])
 
     useEffect(() => {
         setGroupingUnits([
@@ -58,27 +47,13 @@ function Chart() {
         setOhlc([...tempOhlc]);
         setVolume([...tempVolume]);
 
-        binance.candlesticks("BNBBTC", "12h", function (error, ticks) {
-            if (error) {
-                console.log(error)
-            } else {
-                console.log("candlesticks()", ticks);
-                let last_tick = ticks[ticks.length - 1];
-                const temp = ticks.map(item => {
-                    let [time, open, high, low, close, volume] = last_tick;
-                    return [time, open, high, low, close, volume];
-                });
-                console.log("candlesticks()", temp);
-
-            }
-        });
+        console.log("dataa", data)
     }, [data]);
 
 
 
     useEffect(() => {
         if (ohlc.length > 0 && volume.length > 0 && groupingUnits.length > 0) {
-            setReady(true);
         }
     }, [ohlc, volume, groupingUnits])
 
